da8e299befb2ae363c64da011249acc83c28ff1b
sunjolee <sunjosj@hotmail.com>
2014-10-20 23:14:54 +0900

    20141020

---
+++ icm.Framework/Extention.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace icm.Framework
+{
+    public static class Extention
+    {
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+    }
+}
